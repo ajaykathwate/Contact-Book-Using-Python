e24be97cc27c8df9c7c5e4dc7313c09b60f80108
Ajay Kathwate <ajaykathwate13@gmail.com>
2021-09-13 22:50:59 +0530

    Contact Book project created

---
+++ contact_book.py
@@ -0,0 +1,83 @@
+class ContactBook:
+    def __init__(self):
+        self.names = []
+        self.phone_numbers = []
+        
+    def contactBook(self):
+        print("*****Welcome to Contact Book****".center(35))
+        if (len(self.names) > 0):
+            print("\nName\t\t\tPhone Number\n")
+
+            for i in range(len(self.names)):
+                print("{}\t\t\t{}".format(self.names[i], self.phone_numbers[i]))
+        else:
+            print("")
+        print("Which operation would you like to perform: ")
+        self.repeat()
+        
+    def display_contact(self):
+        print("*****Contacts****".center(25))
+        if (len(self.names) > 0):
+            print("\nName\t\t\tPhone Number\n")
+
+            for i in range(len(self.names)):
+                print("{}\t\t\t{}".format(self.names[i], self.phone_numbers[i]))
+        else:
+            print("\nName\t\t\tPhone Number\n")
+            print("---- ------\t\t----------")
+            print("\nResult: No contacts avialable")
+        self.repeat()
+            
+    def insert_contact(self):
+        no_of_contacts = int(input("No of contacts you wants to insert: "))
+        for _ in range(no_of_contacts):
+            name = input("Name: ")
+            phone_number = input("Phone Number: ")
+            self.names.append(name)
+            self.phone_numbers.append(phone_number)
+        self.repeat()
+            
+    def search_contact(self):
+        if len(self.names) > 0:
+            search_term = input("\nEnter search term: ")
+
+            print("Search result: ")
+            if search_term in self.names:
+                index = self.names.index(search_term)
+                phone_number = self.phone_numbers[index]
+                print("Name {}, Phone Number: {}".format(search_term, phone_number))
+            else:
+                print("Name Not found")
+        else:
+            print("\nName\t\t\tPhone Number\n")
+            print("---- ------\t\t----------")
+            print("\nResult: No contacts exists to search")
+        
+    def terminate(self):
+        return None
+    
+    def repeat(self):
+        print("\n")
+        print("-----------------------------------".center(35))
+        print("   Operations")
+        print("1. Display Contact Book\n2. Insert contacts\n3. Search contact by name or number\n4. exit")
+        choice = int(input("Enter choice: "))
+        if choice == 1:
+            self.display_contact()
+        elif choice == 2:
+            self.insert_contact()
+        elif choice == 3:
+            self.search_contact()
+        elif choice == 4:
+            self.terminate()
+        else:
+            print("Not valid input!!!")
+            self.repeat()
+            
+    def previousMove(self):
+        pass
+    
+            
+if __name__=="__main__":
+    cb = ContactBook()
+    cb.contactBook()
